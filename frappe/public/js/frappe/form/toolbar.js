fdd7bcd0d7164050a8ab7896be8a7b31c8c5b821
Caleb Kandoro <kandoroc@hotmail.com>
2022-03-27 13:07:33 +0200

    print permission check remove

--- frappe/public/js/frappe/form/toolbar.js
+++ frappe/public/js/frappe/form/toolbar.js
@@ -175,38 +175,11 @@ frappe.ui.form.Toolbar = Class.extend({
 		var allow_print_for_cancelled = cint(print_settings.allow_print_for_cancelled);
 
 		// Print
-		if(!is_submittable || docstatus == 1  ||
-			(allow_print_for_cancelled && docstatus == 2)||
-			(allow_print_for_draft && docstatus == 0)) {
-			if(frappe.model.can_print(null, me.frm) && !issingle) {
-				if(frappe.boot.features && frappe.boot.features.print_controls) { 
-					// check if user can reprint 
-					frappe.call({
-						method: 'goprime.goprime_erp.print_controls.get_print_permissions',
-						args: {
-							doctype: me.frm.doc.doctype,
-							name: me.frm.doc.name,
-						}
-					}).then(res => {
-						if(res.message.can_print) {
-							me.page.add_menu_item(__("Print"), function() {
-								me.frm.print_doc();}, true);
-							me.print_icon = me.page.add_action_icon("fa fa-print", function() {
-								me.frm.print_doc();
-							});
-						}
-					})
-				}else {
-					me.page.add_menu_item(__("Print"), function() {
-						me.frm.print_doc();}, true);
-					me.print_icon = me.page.add_action_icon("fa fa-print", function() {
-						me.frm.print_doc();
-					});
-				}
-
-				
-			}
-		}
+		me.page.add_menu_item(__("Print"), function() {
+			me.frm.print_doc();}, true);
+		me.print_icon = me.page.add_action_icon("fa fa-print", function() {
+			me.frm.print_doc();
+		});
 
 		// email
 		if(frappe.model.can_email(null, me.frm) && me.frm.doc.docstatus < 2) {
